e9cfc28b777d91f6bdf34a47bf1f909938aa6574
Vladislav <novitsky.vladislav00@gmail.com>
2020-11-03 13:46:25 +0200

    Fixed accept select

--- app/Http/Controllers/FriendsInviteController.php
+++ app/Http/Controllers/FriendsInviteController.php
@@ -114,8 +114,8 @@ class FriendsInviteController extends Controller
     public function accept(Request $request)
     {
         $friends_invite = Friends::where([
-            'user_id' => $request->user()->id,
-            'friends_id' => $request->friends_id
+            'user_id' => $request->friends_id,
+            'friends_id' => $request->user()->id
         ]);
         $friends_invite->accepted = true;
         $friends_invite->save();
