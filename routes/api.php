04aaa228120329041dfa01a36e0c77d4f717739b
Vladislav <novitsky.vladislav00@gmail.com>
2020-11-03 13:33:48 +0200

    Returned auth to /api prefix

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function () {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
 Route::group([
     'prefix' => 'friends'
 ], function () {
